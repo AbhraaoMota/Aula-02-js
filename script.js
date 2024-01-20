10594632339c6bbce51b85701b2a7eda817d5a5d
AbhraaoMota <149906527+AbhraaoMota@users.noreply.github.com>
2024-01-20 13:34:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,151 @@
+// let idade = Number(prompt("Digite sua idade"))
+
+// if(idade >= 18 && idade <=65 ){
+//     console.log("ja pode ser preso")
+//     alert("ja pode ser preso")
+// }
+// else{
+//     console.log("nao pode ser preso")
+//     alert("nao pode ser preso")
+
+// }
+
+// Aprovado e reprovado
+// let media = Number(prompt("digiite a media das notas"))
+//  if(media >= 7 && media < 10){
+//     console.log("Aprovado")
+//   }
+//  else if(media >= 0 && media < 7){
+//     console.log("reprovado")
+// }
+// else if(media === 10){
+//     console.log("Aprovado com distinção")
+// }
+// else{
+//     console.log("media invalida")
+// }
+
+// positivio e negativo
+// let num1 = Number(prompt("Ditite um numero")) 
+
+// if(num1 > 0 ){
+//     alert("Numero positivo")
+// }
+// else if(num1 < 0){
+//     alert("Numero negativo")
+// }
+// else if(num1 == 0){
+//     alert("Numero neutro")
+// }
+// else{
+//     alert("Nao é um numero")
+// }
+
+// Atividade 01
+// let num = Number(prompt("Digite um numero"))
+// let a = num % 2 === 0
+    
+// if(a && num >=0 ){
+//         alert("O numero é par. e ele é positivo")
+//     }
+//     else if(!a && num >= 0){
+//         alert("o numero é impar, e ele é positivo")
+//     }
+//     else if(a && num <= 0){
+//         alert(" O numero é par, e ele é negativo ")
+//     }
+//     else if(!a && num <= 0){
+//         alert("O numero é impar, e ele é negativo")
+//     }
+
+// Atividade 2
+// let peso = Number(prompt("Digite sue peso"))
+// let altura = Number(prompt("Digite sue altura"))
+
+// let imc = peso / (altura*altura)
+// if(imc < 18.5 ){
+//     alert("Abaixo do peso")
+// }
+// else if( imc > 18.5 && imc <= 24.9){
+//     alert("Peso normal")
+// }
+// else if( imc >= 25 && imc <= 29.9){
+//     alert("Sobrepeso")
+// }
+// else if( imc >= 30 && imc <= 34.9 ){
+//     alert("Obesidade grau 1")
+// }
+// else if( imc >= 35 && imc <= 39.9){
+//     alert("Obesidade grau 2")
+// }
+// else if(imc > 40){
+//     alert("Obesidade grau 3")
+// }
+
+// Atividade 3
+// let prod = Number(prompt("Digite o valor do produto"))
+// let idade = Number(prompt("Digite sua idade para um possivel desconto"))
+// let desconto = prod * 0.1
+// let final = prod - desconto
+
+
+// if( idade < 18 ){
+//     alert("voce ganhou o desconto de R$ "+ desconto + ", o valor ficara em R$" + final)
+
+// }else if( idade >= 18 ){
+//     alert("Infelimente nao sera possivel o desconto e o valor ficara em R$" + prod)
+// }
+
+// Atividade 4
+// let lado1 = Number(prompt("Digite o comprimento do seu triangulo"))
+// let lado2 = Number(prompt("Digite o comprimento do seu triangulo"))
+// let lado3 = Number(prompt("Digite o comprimento do seu triangulo"))
+
+// if(lado1 == lado2 && lado1 == lado3 ){
+//     alert("Triângulo equilátero")
+// }
+// else if(lado1 == lado2 && lado1 !==)
+// -------TERMINAR---------------
+
+
+
+
+
+
+
+
+
+
+
+
+// Atividade 6
+// let dia = Number(prompt("Digite de 1 a 7 para um dia na semana"))
+
+// switch(dia){
+//     case 1:
+//         alert("Domingo")
+//         break
+//     case 2:
+//         alert("Segunda-feira")
+//         break
+//     case 3:
+//         alert("Terça-feira")
+//         break
+//     case 4:
+//         alert("Quarta-feira")    
+//         break
+//     case 5:
+//         alert("Quinta-feira")
+//         break
+//     case 6:
+//         alert("Sexta-feira")
+//         break
+//     case 7:
+//         alert("Sabado")
+//     default:
+//         alert("é so de 1 a 7 ")
+
+// }
+
+
+
